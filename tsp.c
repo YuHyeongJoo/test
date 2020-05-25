dd39ba62bd7c21076ae658cea77fb92f76190c83
YuHyeongJoo <21700473@handong.edu>
2020-05-25 18:46:30 +0900

    5_25_6.46pm

--- tsp.c
+++ tsp.c
@@ -13,7 +13,7 @@ typedef struct route_and_distance{
 	char best_route_uptonow[200]; 
 }rad;
 rad ans = {2000000008};
-int n=0,process_count =0, process_limit, parent_pid, id[2];
+int n=0,process_count =0, process_limit, parent_pid, finish[2];
 unsigned long long route_count =0;
 int dist[50][50],consumerid[8] , fd[2],child_id[2], rad_data[2], subtask_done[2], child_done[2];
 bool visited[50] ={true,};
@@ -29,14 +29,16 @@ void sigint_handler(int sig);
 rad init_arr(rad arr);
 int main(int argc, char *argv[] ){
 	parent_pid = getpid();
-	signal(SIGCHLD,handler);
+//	signal(SIGCHLD,handler);
 	signal(SIGINT,sigint_handler);
 	rad pref;
 	pref = init_arr(pref);
 	pref.distance= 0;
 	pipe(fd);
 	pipe(rad_data);
-	pipe(child_id);
+	pipe(finish);
+	pipe(subtask_done);
+	pipe(child_done);
 	FILE *fp = fopen(argv[1],"r");
 	char strTemp[255];
 	fgets(strTemp,sizeof(strTemp),fp);
@@ -59,8 +61,15 @@ int main(int argc, char *argv[] ){
 	ans=init_arr(ans);
 	int init_pid,menu;
 	init_pid=fork();
-	if(init_pid==0)
+	if(init_pid==0){
 		parent(n-11,pref);
+		int child_done_int =0;
+		for(int i=0; i<8; i++){
+			write(child_done[1],&child_done_int.sizeof(int));	
+		}
+		exit(0);
+
+	}
 	while(1){
 		printf("Input number 1~4 1 : stat, 2: treads, 3: num N 4: quit ");
 		scanf("%d",&menu);
@@ -161,31 +170,38 @@ void parent(int k, rad arr){
 		if(process_count<process_limit){
                         pid = fork();
 			if(pid!=0){
+				int child_done_int=1;
 				consumerid[process_count] = pid;
+				write(child_done[1], &child_done_int,sizeof(int));
+				int tmp_dis = arr.distance;
+				char tmp_route[200];
+				strcpy(tmp_route,int_to_string(arr));
+                 	      	 process_count++;
+				write(rad_data[1],&tmp_dis,sizeof(tmp_dis));
+				write(rad_data[1], tmp_route,200);
 			}
-			int tmp_dis = arr.distance;
-			char tmp_route[200];
-			strcpy(tmp_route,int_to_string(arr));
-                        process_count++;
-			write(process_id[1],&pid,sizeof(pid));
-			write(rad_data[1],&tmp_dis,sizeof(tmp_dis));
-			write(rad_data[1], tmp_route,200);
-			
 		}
 		else{
-			int subtask;
+			int child_done_int =1, subtask;
+			
 			char subtask_route[200];
-			while(1){
-				read(subtask_done[0],&subtask, sizeof(int));
-				read(subtask_done[0],subtask_route, 200);
-				if(ans.distance>subtask){
-					ans.distance= subtask;
-					strcpy(ans.best_route_uptonow,subtask_route);
-				}
+			write(child_done[1], &child_done_int, sizeof(int));
+			read(subtask_done[0],&subtask, sizeof(int));
+			read(subtask_done[0],subtask_route, 200);
+			if(ans.distance>subtask){
+				ans.distance= subtask;
+				strcpy(ans.best_route_uptonow,subtask_route);
 			}
+			subtask = arr.distance;
+			strcpy(subtask_route,int_to_string(arr);
+			write(rad_data[1],&subtask,sizeof(int));
+			write(rad_data[1], subtask_route,200);
 		}
 		if(pid ==0){
 			while(1){
+				int child_done_int;
+				read(child_done[0],&child_done_int, sizeof(int));
+				if(child_done_int ==0) exit(0);
 				rad tmp;
 				int tmp_dis;
 				char tmp_route[200];
@@ -198,12 +214,6 @@ void parent(int k, rad arr){
 				strcpy(tmp_route,int_to_string(return_rad));
 				write(subtask_done[1],&tmp_dis,sizeof(int));
 				write(subtask_done[1],tmp_route,200);
-                  	        int current_distance = tmp.distance;
-                        	write(fd[1],&current_distance,sizeof(int));
-                        	char route_string[200];
-                       		strcpy(route_string,int_to_string(tmp));
-                        	write(fd[1], route_string,200);
-                        	exit(0) ;
 
 			}
 		}
